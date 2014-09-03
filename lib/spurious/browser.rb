ce12a700c63f3bec185cedb0d826786fd7429b0b
Steven Jack <stevenmajack@gmail.com>
2014-09-03 19:55:10 +0100

    Initial commit

---
+++ lib/spurious/browser.rb
@@ -0,0 +1,7 @@
+require "spurious/browser/version"
+
+module Spurious
+  module Browser
+    # Your code goes here...
+  end
+end
---
+++ lib/spurious/browser/version.rb
@@ -0,0 +1,5 @@
+module Spurious
+  module Browser
+    VERSION = "0.0.1"
+  end
+end
