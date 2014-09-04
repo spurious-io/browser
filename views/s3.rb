f05471497b832728be546a157ed0195526d30174
Steven Jack <stevenmajack@gmail.com>
2014-09-04 18:09:42 +0100

    S3 WIP

--- views/s3.rb
+++ views/s3.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 class App
   module Views
     class S3 < Layout
@@ -6,6 +8,11 @@ class App
       end
 
       def buckets
+        url = URI.parse("http://#{AWS.config.s3_endpoint}:#{AWS.config.s3_port}")
+        req = Net::HTTP::Get.new(url.to_s)
+        doc  = Nokogiri::XML(req.body)
+        require 'pry'
+        binding.pry
         AWS::S3.new.buckets.to_a
       end
 
