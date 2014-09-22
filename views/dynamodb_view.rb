e6ee175867de1362277588405bf3765a899ace5e
Steven Jack <stevenmajack@gmail.com>
2014-09-22 07:55:52 +0100

    Pagination work

--- views/dynamodb_view.rb
+++ views/dynamodb_view.rb
@@ -4,22 +4,32 @@ class App
   module Views
     class DynamodbView < Layout
 
+      LIMIT = 2
+
+      def item_count
+        AWS::DynamoDB::Client::V20120810.new.describe_table({
+          :table_name => table_name
+        })[:table][:item_count]
+      end
+
       def title
         "#{super} | DynamoDB - View"
       end
 
-      def table_meta
-        ddb = AWS::DynamoDB::Client::V20120810.new
-        ddb.list_tables.table_names.map do |table_name|
-          ddb.describe_table({ :table_name => table_name })[:table]
-        end
+      def table_name
+        @params['table_name']
       end
 
       def items
-        items = AWS::DynamoDB::Client::V20120810.new.scan({
+        options = {
           :table_name => @params['table_name'],
-          :limit      => 10
-        })
+          :limit      => LIMIT
+        }
+
+        options[:exclusive_start_key] = JSON::parse(URI::decode(@params['next_key'])) unless @params['next_key'].nil?
+puts options.inspect
+
+        items = AWS::DynamoDB::Client::V20120810.new.scan(options)
 
         headers = []
 
@@ -27,15 +37,29 @@ class App
 
         item_arr = items[:member].reduce([]) do |accum, current|
           new_accum = current.reduce([]) do |accum_in, (key, value)|
-            accum_in <<  value.values.first
+
+            accum_in << "<td>#{value.values.first}</td>"
           end
-          accum << new_accum
+          accum << new_accum.join("\n")
           accum
         end
+        items[:member].first.delete("location")
+
         {
-          :headers => headers,
-          :data   => item_arr
+          :headers    => headers,
+          :data       => item_arr,
+          :start_key  => items.fetch(:last_evaluated_key, nil),
+          :first_item => items[:member].last
         }
+      end
+
+      def pagination
+        pagination_data = items
+        {
+          :previous => @params['prev_key'].nil? ? '' : "?next_key=#{URI::encode(@params['prev_key'])}",
+          :next => pagination_data[:start_key].nil? ? '' : "?prev_key=#{URI::encode(pagination_data[:first_item].to_h.to_json)}&next_key=#{URI::encode(pagination_data[:start_key].to_h.to_json)}"
+        }
+
       end
 
     end
