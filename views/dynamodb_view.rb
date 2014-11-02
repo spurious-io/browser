666328f66979775878801b119ea1b904b324af95
Steven Jack <stevenmajack@gmail.com>
2014-11-02 17:05:31 +0000

    Adds pagination

--- views/dynamodb_view.rb
+++ views/dynamodb_view.rb
@@ -1,13 +1,19 @@
 require 'aws-sdk'
+require "base64"
 
 class App
   module Views
     class DynamodbView < Layout
+      attr_reader :client
 
-      LIMIT = 2
+      LIMIT = 10
+
+      def client
+        @client ||= AWS::DynamoDB::Client::V20120810.new
+      end
 
       def item_count
-        AWS::DynamoDB::Client::V20120810.new.describe_table({
+        @item_count ||= client.describe_table({
           :table_name => table_name
         })[:table][:item_count]
       end
@@ -20,51 +26,60 @@ class App
         @params['table_name']
       end
 
+      def has_records
+        item_count > 0
+      end
+
       def items
         options = {
-          :table_name => @params['table_name'],
+          :table_name => table_name,
           :limit      => LIMIT
         }
 
-        options[:exclusive_start_key] = JSON::parse(URI::decode(@params['next_key'])) unless @params['next_key'].nil?
-puts options.inspect
-
-        items = AWS::DynamoDB::Client::V20120810.new.scan(options)
+        options[:exclusive_start_key] = pagination_data['next'] unless pagination_data.nil? || pagination_data['next'].nil?
+        items = client.scan(options)
 
         if items[:count] != 0
-
-          headers = []
-
-          items[:member].first.each { |key, data| headers << key }
+          headers = items[:member].first.reduce([]) { |accum, (key, data)| accum << key }
 
           item_arr = items[:member].reduce([]) do |accum, current|
             new_accum = current.reduce([]) do |accum_in, (key, value)|
-
               accum_in << "<td>#{value.values.first}</td>"
             end
-            accum << new_accum.join("\n")
-            accum
+            accum.tap { |a| a << new_accum.join("\n") }
           end
           items[:member].first.delete("location")
 
-          {
+          @data ||= {
+            :first      => pagination_data.nil? || pagination_data['next'].nil?,
+            :last       => items[:last_evaluated_key].nil?,
             :headers    => headers,
             :data       => item_arr,
-            :start_key  => items.fetch(:last_evaluated_key, nil),
-            :first_item => items[:member].last
+            :start_key  => items.fetch(:last_evaluated_key, nil)
           }
         end
       end
 
       def pagination
-        pagination_data = items
-        {
-          :previous => @params['prev_key'].nil? ? '' : "?next_key=#{URI::encode(@params['prev_key'])}",
-          :next => pagination_data[:start_key].nil? ? '' : "?prev_key=#{URI::encode(pagination_data[:first_item].to_h.to_json)}&next_key=#{URI::encode(pagination_data[:start_key].to_h.to_json)}"
+        previous_data = {
+          :prev => pagination_data.nil? || pagination_data['prev'].nil? ? nil : pagination_data['prev']['p'],
+          :next => pagination_data.nil? || pagination_data['prev'].nil? ? nil : pagination_data['prev']['n']
         }
 
+        next_data = {
+          :prev => pagination_data.nil? || pagination_data['next'].nil? ? nil : { 'n' => pagination_data['next'], 'p' => pagination_data['prev'] },
+          :next => items[:start_key].to_h
+        }
+
+        @page ||= {
+          :previous => "?key=#{Base64.encode64(previous_data.to_json)}",
+          :next     => "?key=#{Base64.encode64(next_data.to_json)}"
+        }
       end
 
+      def pagination_data
+        @pagination_data ||= (@params['key'] ? JSON.parse(Base64.decode64(@params['key'])) : nil)
+      end
     end
   end
 end
